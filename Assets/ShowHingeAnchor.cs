69515ec1e843eb8c2d0fdc022773f1d8bb3bd74a
Jack Dibden <s5083071@bournemouth.ac.uk>
2018-05-15 11:33:00 +0100

    Added Pipes, tables and other models.

---
+++ Assets/ShowHingeAnchor.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(HingeJoint))]
+public class ShowHingeAnchor : MonoBehaviour {
+	public float Radius = 0.1f;
+	public float Length = 1.0f;
+	public Color SphereColor = Color.green;
+	public Color LineColor = Color.green;
+	public bool Wireframe = false;
+	
+	void OnDrawGizmos() {
+		var hinge = GetComponent<HingeJoint>();
+		
+		var dir = hinge.axis;
+		var ws = transform.localToWorldMatrix * hinge.anchor;
+		var pos = transform.position + new Vector3(ws.x, ws.y, ws.z);
+
+		Gizmos.color = LineColor;
+		Gizmos.DrawLine(pos, pos + dir * Length);
+		
+		Gizmos.color = SphereColor;
+		if(Wireframe) {
+			Gizmos.DrawWireSphere(pos, Radius);
+		} else {
+			Gizmos.DrawSphere(pos, Radius);
+		}
+	}
+}
