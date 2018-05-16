08d62c5276143b02e723bdd98bd42977b815df4e
Smallsplat <Jack.dibden@gmail.com>
2018-05-16 16:33:37 +0100

    Added the car and the 3 shelf objects!

---
+++ Assets/Car_Motor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car_Motor : MonoBehaviour
+{
+    public float movementSpeed = 10;
+
+    void Update()
+    {
+
+        transform.Translate(Vector3.forward * movementSpeed * Time.deltaTime);
+
+    }
+}
