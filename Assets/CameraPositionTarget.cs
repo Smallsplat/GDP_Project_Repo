46f60d66060f51f6938eeeb79d65be6383f88f72
Jack Dibden <S5083071@bournemouth.ac.uk>
2018-05-18 11:33:35 +0100

    Finnished Camera work, added Gong.

---
+++ Assets/CameraPositionTarget.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+// custom struct that I use in the editor to allow position/target to be set at the same time
+[Serializable]
+public struct CameraPositionTarget {
+	public Transform position;
+	public Transform target;
+    public float Duration;
+    public float TransitionDuration;
+
+	public Vector3 direction() {
+		return (target.position - position.position).normalized;
+	}
+}
