711c6bc165c038ed5a68c4bf68a0156323f2a54b
Smallsplat <Jack.dibden@gmail.com>
2018-05-19 11:18:31 +0100

    Gong now plays

---
+++ Assets/PlayGong.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//Add this Script Directly to The Death Zone
+public class PlayGong : MonoBehaviour
+{
+    public AudioClip Gong;
+
+    void Start()
+    {
+        GetComponent<AudioSource>().playOnAwake = false;
+        GetComponent<AudioSource>().clip = Gong;
+    }
+
+    void OnCollisionEnter()
+    {
+        GetComponent<AudioSource>().Play();
+    }
+}
